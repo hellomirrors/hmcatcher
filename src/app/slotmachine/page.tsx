4fc5a8b79066bec30f88f275959535eb57380f1b
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2026-03-31 22:03:45 +0200

    Add contact form with validation and submission handling

---
+++ src/app/slotmachine/page.tsx
@@ -0,0 +1,9 @@
+import { ContactForm } from "./contact-form";
+
+export default function KontaktdatenPage() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-4">
+      <ContactForm />
+    </div>
+  );
+}
